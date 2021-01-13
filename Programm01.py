94b48174af57eedf98663dad5c7168f8d7f3187b
f0r3idd3n-n3tw0rk2 <77199336+f0r3idd3n-n3tw0rk2@users.noreply.github.com>
2021-01-13 22:26:28 +0100

    Update Programm01.py

--- Programm01.py
+++ Programm01.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+
 import sys
 
 def print5time(line_to_print):
+    
     for count in range(0,5):
-        print(line_to_print)
+            print(line_to_print)
 
 print5time(sys.args[1])
