ef97c55af02ffbff5c82e76afefb997b93507f09
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-20 23:09:26 +0100

    xss_scanner commit07 start concept and program structure

--- Python01_xss_scanner/xss_scanner.py
+++ Python01_xss_scanner/xss_scanner.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 
+import requests
+
+r = requests.post('$host', data = {'field1':'name'})
+
+payload = {'field1': 'name', 'field2': 'street'}
+r = requests.post('$host', params = payload)
