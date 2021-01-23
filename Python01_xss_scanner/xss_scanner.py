c5b0c2dab91e35b5fc742e48e098753552fe7fea
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-23 18:38:50 +0100

    xss_scanner commit13 start program structure and arguments parser

--- Python01_xss_scanner/xss_scanner.py
+++ Python01_xss_scanner/xss_scanner.py
@@ -45,7 +45,7 @@ def xss_finder():
    try:
        print(bcolors.CBLUE,"e.g target ->>>> http://target.com/submit.php?name=")
        url = input("Please Enter Target URL\t:")
-       print(bcolors.CBLUE,"Default Payload List ->>>> xss_teststrings.txt")
+       print(bcolors.CBLUE,"Default Payload List ->>>> xss_teststrings02.txt")
        y = """
        1) BASIC PAYLOAD TESTSTRINGS
        2) WORDLIST WFUZZ XSS
@@ -55,7 +55,7 @@ def xss_finder():
        print(y)
        choice = input("->>>>Please Choose Payload List\t:")
        if choice == '1':
-           print("Selected Payload payload:xss_teststrings.txt\n")
+           print("Selected Payload payload:xss_teststrings02.txt\n")
            choice = "xss_teststrings.txt"
        elif choice == '2':
            print("Selected Payload payload: /usr/share/wordlists/wfuzz/Injections/XSS.txt")
