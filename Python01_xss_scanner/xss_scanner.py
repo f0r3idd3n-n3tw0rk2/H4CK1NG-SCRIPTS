4e48a269e80da12311e80d05a47fda762eb1799e
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-23 18:05:28 +0100

    xss_scanner commit10 start program structure and arguments parser

--- Python01_xss_scanner/xss_scanner.py
+++ Python01_xss_scanner/xss_scanner.py
@@ -6,6 +6,13 @@ import requests
 import argparse
 import sys
 import logging
+import random
+
+
+
+class bcolors:
+    CBLUE = '\33[34m'
+    CRED = '\33[31m'
 
 
 
@@ -47,9 +54,92 @@ if not args.field:
     sys.exit(1)
 
 
-if args.https:
-    logging.info("Importing ssl module")
-    import ssl
+
+
+
+
+#Funktion XSS Finder User Agents für die User Agents Eingabe im Programm ausgewählt aus der Liste (useragent.txt)
+def get_user_agent():
+    try:
+        lines = [line.rstrip("\n") for line in open("useragent.txt")]
+    except IDError as e:
+        print("User Agent error: %s" % e.strerror)
+        sys.exit(1)
+    return lines
+
+
+
+
+
+
+
+
+
+#Funktion XSS Finder Auswahl für die Standartliste, XSS Infections wfuzz Wordlist oder eine selbsterstellte Liste
+
+def xss_finder():
+   try:
+       print(bcolors.CBLUE,"e.g target ->>>> http://target.com/submit.php?name=")
+       url = input("Please Enter Target URL\t:")
+       print(bcolors.CBLUE,"Default Payload List ->>>> xss_teststrings.txt")
+       y = """
+       1) BASIC PAYLOAD TESTSTRINGS
+       2) WORDLIST WFUZZ XSS
+       3) ENTER FILE PATH
+       4) EXIT
+       """
+       print(y)
+       choice = input("->>>>Please Choose Payload List\t:")
+       if choice == '1':
+           print("Selected Payload payload:xss_teststrings.txt\n")
+           choice = "xss_teststrings.txt"
+       elif choice == '2':
+           print("Selected Payload payload: /usr/share/wordlists/wfuzz/Injections/XSS.txt")
+           choice = input("Path enter\t:")
+       elif choice == '3':
+           print("Selected Payload payload: /PATH/TO/YOUR/PAYLOAD/XSS.txt")
+           choice = input("Path enter\t:")
+       elif choice == '4':
+           print("Exiting....")
+           sys.exit()
+       else:
+           print("Wrong Choice...")
+           choice = choice.replace("\\", "/")
+       while True:
+           with open(choice, "r", errors="replace") as f:
+               for i in f:
+                   try:
+                       usrr = get_user_agent()
+                       header = {"User-Agent": "{}".format(random.choice(usrr))}
+                       req = requests.get(url + i, headers=header)
+                       if i in req.txt:
+                           print(bcolors.CRED,"Parameter vulnerable\r\n")
+                           print(bcolors.CRED,"Vulnerable Payload Found\t: " + req.url)
+                           with open("vulnpayload.txt", "a+") as ss:
+                               ss.write(i)
+
+                       else:
+                           print(bcolors.CBLUE,"Trying\t:", req.url)
+                   except:
+                       pass
+               break
+
+       
+    except Exception as err:
+        print(err)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
