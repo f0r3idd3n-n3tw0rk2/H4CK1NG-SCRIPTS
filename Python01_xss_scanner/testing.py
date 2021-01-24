97a7cf3ed6b5e1518b7a74d0ca6ccc428f5d3782
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-24 14:03:54 +0100

    xss_scanner commit22 testing

--- Python01_xss_scanner/testing.py
+++ Python01_xss_scanner/testing.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
 import requests
+from BeautifulSoup import BeautifulSoup
 
 def request(url):
+
     try:
-        return requests.get("http://" + url)
+        return requests.get(url)
     except requests.exception.ConnectionError:
         pass
+
+
+
+target_url = "http://167.71.54.69/"
+response = request(target_url)
+print(response.content)
