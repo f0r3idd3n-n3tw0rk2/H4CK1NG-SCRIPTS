59ee387b47adcfcbef658dfbf28fe34ee91ffc93
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-24 18:49:05 +0100

    python04 01 response and print code (40,41

--- Python02_vuln_scanner/python04_posting.py
+++ Python02_vuln_scanner/python04_posting.py
@@ -37,3 +37,5 @@ for form in forms_list:
         post_data[input_name] = input_value
     result = requests.post(post_url, data=post_data)
     print(result.content)
+    if result.content is not "&lt, &gt":
+        print("Webpage is vulnerable")
