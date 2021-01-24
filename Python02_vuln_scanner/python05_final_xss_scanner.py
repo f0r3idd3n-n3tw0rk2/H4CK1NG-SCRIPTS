fbbde7e2babde05b96b5a4e38b3483e1ae921534
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-24 23:19:14 +0100

    python05 final

--- Python02_vuln_scanner/python05_final_xss_scanner.py
+++ Python02_vuln_scanner/python05_final_xss_scanner.py
@@ -62,7 +62,7 @@ for form in forms_list:
                     print(bcolors.CBLUE, "\r\n[+++++]", "Webpage has no XSS Vulnerability", "[+++++]")
                 else:
                     print(bcolors.CRED, "\r\n[-----]", "Webpage has an XSS Vulnerability", "[-----]")
-                    print(bcolors.CRED, "\r\n[Form_Field:]", input_type, "\r\n[---------------------------]")
+                    print(bcolors.CRED, "\r\n[---------------------------]", "\r\n[Form_Field:]", input_type, "\r\n[---------------------------]")
 
 
 
