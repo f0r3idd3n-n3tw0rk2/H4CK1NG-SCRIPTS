b68d87f07cdfa40a5e7accf23c05c66bd4f79da2
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-24 14:17:44 +0100

    vuln_scanner 01 extract_forms

---
+++ Python02_vuln_scanner/python01_extract_forms.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import requests
+from bs4 import BeautifulSoup
+
+def request(url):
+
+    try:
+        return requests.get(url)
+    except requests.exception.ConnectionError:
+        pass
+
+
+
+target_url = "http://167.71.54.69/"
+response = request(target_url)
+print(response.content)
