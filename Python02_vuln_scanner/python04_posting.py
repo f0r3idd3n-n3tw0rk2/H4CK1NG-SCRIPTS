10d5739c0ac6c53437e38b40cbbfcffed419e7c2
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-24 18:41:30 +0100

    python04 01 response and print code (40,41

--- Python02_vuln_scanner/python04_posting.py
+++ Python02_vuln_scanner/python04_posting.py
@@ -37,5 +37,5 @@ for form in forms_list:
         post_data[input_name] = input_value
     result = requests.post(post_url, data=post_data)
     print(result.content)
-    response = form(forms_list[0], "testtest", target_url)
+    response = inputs_list(forms_list[0], "testtest", target_url)
     print(response.content)
