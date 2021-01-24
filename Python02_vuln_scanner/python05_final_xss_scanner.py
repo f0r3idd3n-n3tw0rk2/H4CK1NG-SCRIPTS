14646f8a13335111c090949f6466ccc350e1d1be
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-24 23:44:59 +0100

    python05 final

--- Python02_vuln_scanner/python05_final_xss_scanner.py
+++ Python02_vuln_scanner/python05_final_xss_scanner.py
@@ -39,7 +39,7 @@ for form in forms_list:
         if input_type == "text":
             input_value = "<h1>test</h1>"
         # print(input)
-        for Post in input_value:
+        for Post in post_data:
             post_data[input_name] = input_value
             print(Post)
 
