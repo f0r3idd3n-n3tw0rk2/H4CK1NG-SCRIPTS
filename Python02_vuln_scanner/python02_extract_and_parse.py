52888675c9c2ec487cff411c2ed5b4e774c179b4
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-24 14:41:42 +0100

    vuln_scanner 04 extract_forms and parse

--- Python02_vuln_scanner/python02_extract_and_parse.py
+++ Python02_vuln_scanner/python02_extract_and_parse.py
@@ -20,5 +20,5 @@ target_url = "http://167.71.54.69/"
 response = request(target_url)
 
 parsed_html = BeautifulSoup(response.content, "html.parser")
-forms_list = parsed_html.findAll("forms")
+forms_list = parsed_html.findAll("form")
 print(forms_list)
