fdb8f5d5c233ef97f3d51d63f1583ce5b201b7a1
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-24 23:28:49 +0100

    python05 final

--- Python02_vuln_scanner/python05_final_xss_scanner.py
+++ Python02_vuln_scanner/python05_final_xss_scanner.py
@@ -43,7 +43,7 @@ for form in forms_list:
 
             post_data[input_name] = input_value
 
-    for input_type in forms_list:
+    for input_type in post_data:
         #print(input_value)
         result = requests.post(post_url, data=post_data)
         result_p_content = print(result.content)
