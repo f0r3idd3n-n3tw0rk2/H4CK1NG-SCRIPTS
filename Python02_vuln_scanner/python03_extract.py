38b55243974faf2c4b396376caf32df2a1d81dd7
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-24 15:00:28 +0100

    vuln_scanner 05 extract and readme

--- Python02_vuln_scanner/python03_extract.py
+++ Python02_vuln_scanner/python03_extract.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
 
-#This is a programm to get the URL and print the response of the html code back
-#Then calling Beautifulsoup and giving it the response content for parsing
-#Get and Print the from Content on the Page, here only one form
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -21,4 +19,6 @@ response = request(target_url)
 
 parsed_html = BeautifulSoup(response.content, "html.parser")
 forms_list = parsed_html.findAll("form")
-print(forms_list)
+
+for form in forms_list:
+    print(form)
