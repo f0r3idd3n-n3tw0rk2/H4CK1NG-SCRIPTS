72417624537f5270398fc25e7ca14b639626c498
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-25 18:01:40 +0100

    python05 final

--- Python02_vuln_scanner/python05_final_xss_scanner.py
+++ Python02_vuln_scanner/python05_final_xss_scanner.py
@@ -15,16 +15,15 @@ class bcolors:
 
 
 def request(url):
-    target_url = "http://167.71.54.69/"
-    response = request(target_url)
+
     try:
         return requests.get(url)
     except requests.exception.ConnectionError:
         pass
 
 
-#target_url = "http://167.71.54.69/"
-#response = request(target_url)
+    target_url = "http://167.71.54.69/"
+    response = request(target_url)
 
     parsed_html = BeautifulSoup(response.content, "html.parser")
     forms_list = parsed_html.findAll("form")
