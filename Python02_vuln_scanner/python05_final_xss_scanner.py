554aac8b786fb86431bd4bd24db9a6eb14eb56ed
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-25 18:04:08 +0100

    python05 final

--- Python02_vuln_scanner/python05_final_xss_scanner.py
+++ Python02_vuln_scanner/python05_final_xss_scanner.py
@@ -23,7 +23,8 @@ def request(url):
         pass
 
 
-
+    target_url = "http://167.71.54.69/"
+    response = request(target_url)
 
     parsed_html = BeautifulSoup(response.content, "html.parser")
     forms_list = parsed_html.findAll("form")
