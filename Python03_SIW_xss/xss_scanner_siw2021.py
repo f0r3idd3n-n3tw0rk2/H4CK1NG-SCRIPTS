9e9fd7fdf1ca88c470ac442303d41c88fb2a348e
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-01-26 20:06:56 +0100

    python03_siw xss_scanner 01

--- Python03_SIW_xss/xss_scanner_siw2021.py
+++ Python03_SIW_xss/xss_scanner_siw2021.py
@@ -7,25 +7,51 @@ import sys
 #print(sys.argv)
 
 
-
-text_file = open("xss_test_string.txt", "r")
-lines = text_file.readlines()
-print (lines)
-print (len(lines))
-text_file.close()
-
-
-
 url = sys.argv[1]
 parameters = sys.argv[2].split(",")
 
-print(url)
-print(parameters)
+
+
+def load_xss_test_strings(filename):
+    print("|+] Starting Scanner")
+    with open(filename) as f:
+        content = f.readline()
+    content = [x.strip() for x in content]
+    return content
+
+
 
 
 def main():
     print("|+] Starting Scanner")
 
+    xss_test_strings = load_xss_test_strings('xss_test_string.txt')
+
+    for xss_test_strings in xss_test_strings:
+        print(xss_test_strings)
+
+
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+#print(url)
+#print(parameters)
+
+#text_file = open("filename.dat", "r")
+#lines = text_file.readlines()
+#print lines
+#print len(lines)
+#text_file.close()
