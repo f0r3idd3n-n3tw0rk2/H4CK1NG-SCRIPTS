399c17a800fa97cba3726194256c9b70110872cb
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-02-01 23:04:51 +0100

    python_program01 example

--- Python05_SIW_Standard/python_program01.py
+++ Python05_SIW_Standard/python_program01.py
@@ -4,6 +4,7 @@
 import sys
 
 
+#Printing Number of Arguments f.eg. python3 scriptname.py argument1 argument2
 
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
@@ -17,10 +18,31 @@ print('Argument List:', str(sys.argv))
 
 
 
+#a List
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+print(mylist[0])
+#apple
+print(len(mylist))
+
+#a dictionary
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+print(thisdict["brand"])
+
 
 #2.) Define variables
 #sys.arv 1 -> python3 "programme-name.py" "sys.arv 1"
 #sys.arv 2 -> python3 "programme-name.py" "sys.arv 2 and split by ,"
+
+txt = "welcome to the SIW"
+x = txt.split()
+print(x)
+
 
 variable1 = sys.argv[1]
 variable2 = sys.argv[2].split(",")
@@ -56,3 +78,30 @@ def main():
      main()
 
 
+
+
+#5.) Define a function with my_function() method
+# in the end call the function
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+
+#6.) Define a function and call the attribute
+def my_function(variable2):
+  print(variable2 + "Hello from a function")
+
+my_function(variable2)
+
+
+#7.) Passing a list in a function as argument
+
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
