24439ada5509924f8d82f46d2c859bd5b6e46520
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-02-03 21:56:36 +0100

    ssh_brute_force_detector VER 01

---
+++ Python06_SIW_Brute-Force-scanner/ssh_brute_force_detector.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+#1.) Import modules
+
+
+
+
+
+#2.) Define the variable
+
+
+
+
+
+
+#3.) Read a Log file and add the content to an array
+
+
+def load_server_logfile(filename):
+    print("[+] Loading Server Logfile")
+    with open(filename) as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
+    return content
+
+
+
+
+#with open("Data.log", "r") as ins:
+
+#    array = []
+
+#    for line in ins:
+
+#        array.append(line)
+
+
+
+
+
+
+#4.) search through the list/array and print only failed logins within 10 seconds
+
+
+
+
+
+#5.) search through the list/array and print only failed logins within on same day and count it
+
+
+
+
+
+#6.) search through the list/array and print only failed logins within on total and count it
+
+
+
+
+
+
+#for element in answered_list:  # for each element from the list in the answer result
+#    client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}  # create a dictionary with ip and mac
+#    clients_list.append(client_dict)  # append the result to clients_list from client dict
+
+#return (clients_list)
+
+
+
+
+#create a function which is doing the printing of that list. Variable results_list
+#def print_result(results_list):
+#    print("IP\t\t\tMAC Address\n-------------------------------------")             #print a header line
+#    for client in results_list:
+#        print(client["ip"] + "\t\t" + client["mac"])                                #print clients only ip and mac
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if xss_test_script not in line:
+    print(bcolors.CBLUE,
+          "\r\n[---------------------------------------------------------------------------]",
+          "\r\n")
+    print(bcolors.CBLUE, "\r\n[---------------------------]", "\r\n[Form_Field:]", input_type,
+          "\r\n[---------------------------]")
+    print(bcolors.CBLUE, "\r\n[+++++]", "Webpage has no XSS Vulnerability", "[+++++]")
+else:
+    print(bcolors.CRED,
+          "\r\n[---------------------------------------------------------------------------]",
+          "\r\n")
+    print(bcolors.CRED, "\r\n[---------------------------]", "\r\n[Form_Field:]", input_type,
+          "\r\n[---------------------------]")
+    print(bcolors.CRED, "\r\n[-----]", "Webpage has an XSS Vulnerability", "[-----]", "\r\n")
