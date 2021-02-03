bcd9fb32cafcbd1dae3536dd48c349903e14ab66
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-02-03 23:13:57 +0100

    ssh_brute_force_detector VER 01

--- Python06_SIW_Brute-Force-scanner/ssh_brute_force_detector02.py
+++ Python06_SIW_Brute-Force-scanner/ssh_brute_force_detector02.py
@@ -8,6 +8,6 @@ from apachelogs import LogParser
 
 
 
-with open('server_logfile_test.txt') as fp:  # doctest: +SKIP
+with open('/server_logfile_test.txt') as fp:  # doctest: +SKIP
     for entry in parser.parse_lines(fp):
         print(str(entry.request_time), entry.request_line)
