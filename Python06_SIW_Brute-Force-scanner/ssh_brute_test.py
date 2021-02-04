4e275145c4c826719bc86e1bd5ede7a838754536
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-02-04 21:07:06 +0100

    ssh_brute_force_detector VER 01

--- Python06_SIW_Brute-Force-scanner/ssh_brute_test.py
+++ Python06_SIW_Brute-Force-scanner/ssh_brute_test.py
@@ -13,7 +13,14 @@ from collections import Counter
 #   Mar 18 13:50:30 bit_server sshd[22625]: Failed password for invalid user abridge from 45.15.16.37 port 18397 ssh2
 #
 
+#Standard function open()
+#deliver a new file object logs and print the output
 
-daten = open('server_logfile_test.txt')
-print(daten.read())
+try:
+    logs = open('server_logfile_test.txt', 'r')
+    serverlog = logs.read()
+    print(serverlog)
+
+except:
+    print('Can not read the logfile')
 
