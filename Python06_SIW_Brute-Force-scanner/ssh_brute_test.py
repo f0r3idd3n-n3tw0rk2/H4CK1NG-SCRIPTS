32ae7ae15071c731875b106cc360cc5793be0779
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-02-06 18:40:14 +0100

    ssh_brute_force_detector VER 02

--- Python06_SIW_Brute-Force-scanner/ssh_brute_test.py
+++ Python06_SIW_Brute-Force-scanner/ssh_brute_test.py
@@ -36,7 +36,7 @@ threshold = 5
 
 
 
-if log_filename == 0:
+if log_filename == None:
     print("[+] Loading Server Logfile")
     print("[+] Using default Logfile in Folder")
     default_logfile = 'server_logfile_test.txt'
