e6d4490949369b84b6db83e57c7a438e8ab8d308
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-02-06 16:08:55 +0100

    ssh_brute_force_detector VER 02

--- Python06_SIW_Brute-Force-scanner/ssh_brute_force_detector02.py
+++ Python06_SIW_Brute-Force-scanner/ssh_brute_force_detector02.py
@@ -1,13 +1,79 @@
 #!/usr/bin/env python3
 
+#1.) Import modules
 
-import apache_log_parser
+import re
+import sys
+import time
+import tqdm
 
-line_parser = apache_log_parser.make_parser("%v %h %l %u %t \"%r\" %>s %b")
-
-filename = 'server_logfile_test.txt'
 
 
-with open(filename) as fp:  # doctest: +SKIP
-    for entry in parser.parse_lines(fp):
-        print(str(entry.request_time), entry.request_line)
+class bcolors:
+    CBLUE = '\33[34m'
+    CRED = '\33[31m'
+    CGREEN = '\33[92m'
+
+
+#2.) Define the variable
+# use argument for input
+
+print(bcolors.CGREEN,"Default Server Logfile must be in folder ->>>> server_logfile_test.txt")
+print(bcolors.CGREEN,"Default Treshold ->>>> treshold -> integer")
+
+log_filename = input("Please Enter the logfile to analyze\t:")
+threshold = 5
+
+
+
+
+#print("Server Logfile will be analyzed please wait....")
+#time.sleep(3.5)    # Pause 5.5 seconds
+
+#for i in tqdm.tqdm(range(200)):
+#    time.sleep(0.01)
+    # or other long operations
+
+
+
+
+#3.) Read a Log file and add the content to an array
+
+
+print("[+] Loading Server Logfile")
+with open(log_filename) as f:
+    content = f.readlines()
+    content = [line.strip() for line in content]
+    print(content)
+return content
+
+
+
+
+
+
+#get lines with login failures
+#extract IP of this line
+#remove duplicate items from list
+#count how many failed login attempts for this ip
+#print the result if it is above the treshold
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
