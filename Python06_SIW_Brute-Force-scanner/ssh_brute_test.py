8f64de88a91b5950e4796a745045d8ca66c01857
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-02-09 18:00:23 +0100

    ssh_brute_force_detector VER 02

--- Python06_SIW_Brute-Force-scanner/ssh_brute_test.py
+++ Python06_SIW_Brute-Force-scanner/ssh_brute_test.py
@@ -151,7 +151,7 @@ else:
             if "pam_unix(sshd:auth): authentication failure;" in item:
                 #print(item)
                 item = item.strip().split(" ")
-            print(item)
+
 
                 ########copy-paste######
             with open(log_filename) as f:
@@ -159,12 +159,13 @@ else:
                 my_iplist = re.findall(ip_myregex, log)
                 ipcount = Counter(my_iplist)
         for k, v in ipcount.items():
-            if str(v) >= threshold:
-                print(bcolors.CRED, "IP Address " + "=> " + str(k) + " " + "Count " + "=> " + str(v))
-                print(bcolors.CRED, "IP-Address\t:", str(k), "Bruteforce Attack", str(k), "is on Blacklist")
-            else:
-                print(bcolors.CGREEN, "IP Address " + "=> " + str(k) + " " + "Count " + "=> " + str(v))
-                print(bcolors.CGREEN, "IP-Address\t:", str(k), "Threshold not reached")
+            for item in log_error:
+                if str(v) >= threshold:
+                    print(bcolors.CRED, "IP Address " + "=> " + str(k) + " " + "Count " + "=> " + str(v))
+                    print(bcolors.CRED, "IP-Address\t:", str(k), "Bruteforce Attack", str(k), "is on Blacklist")
+                else:
+                    print(bcolors.CGREEN, "IP Address " + "=> " + str(k) + " " + "Count " + "=> " + str(v))
+                    print(bcolors.CGREEN, "IP-Address\t:", str(k), "Threshold not reached")
             #copy-paste##
             # end of regex and IP
 
