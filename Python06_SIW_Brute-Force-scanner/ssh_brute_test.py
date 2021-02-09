9497466057d3c02c6af9bae0c668914407e1860e
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-02-09 18:10:23 +0100

    ssh_brute_force_detector VER 02

--- Python06_SIW_Brute-Force-scanner/ssh_brute_test.py
+++ Python06_SIW_Brute-Force-scanner/ssh_brute_test.py
@@ -105,40 +105,6 @@ else:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#open the filename, read through, make a list with all findings of IP adresses in variable log and count it
-    #with open(log_filename) as f:
-        #log = f.read()
-
-        #my_iplist = re.findall(ip_myregex, log)
-        #ipcount = Counter(my_iplist)
-        #for k, v in ipcount.items():
-            #if str(v) >= threshold:
-                #print(bcolors.CRED, "IP Address " + "=> " + str(k) + " " + "Count " + "=> " + str(v))
-                #print(bcolors.CRED, "IP-Address\t:", str(k), "Bruteforce Attack", str(k), "is on Blacklist")
-            #else:
-                #print(bcolors.CGREEN, "IP Address " + "=> " + str(k) + " " + "Count " + "=> " + str(v))
-                #print(bcolors.CGREEN, "IP-Address\t:", str(k), "Threshold not reached")
-#end of regex and IP
-
-
-
-
-
     # regex to search for Line and Error in the file
     # open the filename, read through, make a list with all findings of "Failed" in variable log and count it
     count = 0
@@ -176,10 +142,6 @@ else:
 
 
 
-        #my_errorlist = re.findall(failed_myregex, log_error)
-    #for line in my_errorlist:
-        #count += 1
-        #print("Line{}: {}".format(count, line.strip().split(" ")))
 
 
 
@@ -195,136 +157,10 @@ else:
 
 
 
-            #match_date = re.search(r'Mar$', line)
-            #if match_date is None:
-                #print("No Match")
-            #else:
-                #print(match_date)
 
 
 
-    #with open(default_logfile) as f:
-        #lines = f.read().splitlines()
-        #print(lines[1])
-        #for x in lines:
-            #regex = r"('^\w\w\w\s\d\d$')"
-    #^\w\w\w\w\d:\w\w\w\s\d\d$
-           # match = re.search(regex, 'lines')
-            #if match is None:
-            #    print("No Match")
-            #print(match)
 
 
 
 
-
-    #print(type(lines))
-    #print(lines[0])
-    #print(lines[1])
-    #print(lines[2])
-
-
-
-
-
-
-        #values = {}
-        #for parameter in values:
-            #values[parameter] = lines
-
-            #filtered_values = list(filter(lambda v: match ('^\w\w\w\s\d\d$', v), lines))
-            #result = re.findall('^\w\w\w\s\d\d$', values)
-
-
-
-    #print("Date\t:", str(date)), Regex: '\w\w\w\s\d\d$'
-    #print("Time\t:", str(time))
-    #print("IP-Adress\t:", str(IP))
-
-
-
-
-
-
-
-
-
-
-
-
-    #with open(log_filename) as f:
-       # lines = f.read().splitlines()
-
-    #print(type(lines))
-    #print(lines)
-
-
-    #for x in lines:
-        #values = str(lines)
-        #filtered_values = list(filter(lambda v: match ('^\w\w\w\s\d\d$', v), values))
-        #result = re.findall('^\w\w\w\s\d\d$', values)
-        #print(filtered_values)
-
-
-
-
-
-
-
-
-
-#def search(log_filename, brute_force):
-
-
-    #for i in range(len(lines)):
-        #if lines[i] == brute_force:
-           # return True
-        #return False
-
-   # login_list = lines
-
-
-    #if search(login_list, brute_force):
-       # print("[!] Brute Force Detection")
-    #else:
-        #print("[+] All good. No Brute Force On Server.")
-
-
-#search(brute_force, log_filename)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# i = "root"
-# for i in lines:
-#    print("[!] Brute Force Detection")
-
-# else:
-# Keine Verwundbarkeit gefunden
-#    print("[+] All good. No Brute Force On Server.")
-
-
-# if match:
-# Brute Force Detected
-# print("[!] Brute Force Detection")
-# else:
-# Keine Verwundbarkeit gefunden
-# print("[+] All good. No Brute Force On Server.")
-
-
-# get lines with login failures
-# extract IP of this line                           -> OK
-# remove duplicate items from list
-# count how many failed login attempts for this ip  -> OK
-# print the result if it is above the treshold      -> OK
