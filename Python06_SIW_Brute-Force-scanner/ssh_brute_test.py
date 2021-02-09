d158b6c6bd382d8934c27f2217c18af1f11a6d46
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-02-09 18:07:34 +0100

    ssh_brute_force_detector VER 02

--- Python06_SIW_Brute-Force-scanner/ssh_brute_test.py
+++ Python06_SIW_Brute-Force-scanner/ssh_brute_test.py
@@ -161,12 +161,12 @@ else:
         for items in item:
             for k, v in ipcount.items():
             #for items in item:
-                if str(v) >= threshold:
-                    print(bcolors.CRED, "IP Address " + "=> " + str(k) + " " + "Count " + "=> " + str(v))
-                    print(bcolors.CRED, "IP-Address\t:", str(k), "Bruteforce Attack", str(k), "is on Blacklist")
-                else:
-                    print(bcolors.CGREEN, "IP Address " + "=> " + str(k) + " " + "Count " + "=> " + str(v))
-                    print(bcolors.CGREEN, "IP-Address\t:", str(k), "Threshold not reached")
+            if str(v) >= threshold:
+                print(bcolors.CRED, "IP Address " + "=> " + str(k) + " " + "Count " + "=> " + str(v))
+                print(bcolors.CRED, "IP-Address\t:", str(k), "Bruteforce Attack", str(k), "is on Blacklist")
+            else:
+                print(bcolors.CGREEN, "IP Address " + "=> " + str(k) + " " + "Count " + "=> " + str(v))
+                print(bcolors.CGREEN, "IP-Address\t:", str(k), "Threshold not reached")
             #copy-paste##
             # end of regex and IP
 
