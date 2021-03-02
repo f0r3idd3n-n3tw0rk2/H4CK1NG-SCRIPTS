df33ed7e3a509a8b6e9761e9bbe9bcd6ef8ec2a4
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-03-02 19:00:29 +0100

    pyshark_files 01

--- Python07_SIW_Pyshark/siw_programme01.py
+++ Python07_SIW_Pyshark/siw_programme01.py
@@ -3,7 +3,7 @@ import pyshark
 count = 0
 ip_list = []
 
-capture = pyshark.FileCapture('HTTP_traffic.pcap', display_filter='http && ip.dst ==  146.64.213.83')
+capture = pyshark.FileCapture('data-exfiltration1.pcap', display_filter='http && ip.dst ==  146.64.213.83')
 
 for packet in capture:
     if packet.ip.dst not in ip_list:
