6527190f77aa69688bcd91f13fe77e7b2e24179d
f0r3iddenn3tworks <rene.kretzinger@f0r3idd3n-n3tw0rk2.ch>
2021-03-02 19:18:42 +0100

    pyshark_files 01

--- Python07_SIW_Pyshark/siw_programme01.py
+++ Python07_SIW_Pyshark/siw_programme01.py
@@ -6,8 +6,8 @@ request_uri = []
 capture = pyshark.FileCapture('data-exfiltration1.pcap', display_filter='http && ip.dst ==  146.64.213.83')
 
 for packet in capture:
-    if packet.http.request.uri not in request_uri:
-        request_uri.append(packet.http.request.uri)
+    if packet.http.request not in request_uri:
+        request_uri.append(packet.http.request)
 
 print(request_uri)
 
